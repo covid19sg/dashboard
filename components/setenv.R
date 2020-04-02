f181927ee9a27982c85e7bad78b0bd1ccd572083
Rafi Hesham - Singapore-MR <n169114@as.munichre.com>
2020-04-02 12:07:13 +0800

    Initial Commit

---
+++ components/setenv.R
@@ -0,0 +1,38 @@
+####---- Variable Initialization ----####
+tab01 <- "Global"
+Tab01_1 <- "Dashboard"
+Tab01_2 <- "Trends"
+Tab01_3 <- "Wordcloud"
+tab02 <- "Singapore"
+tabWelcome <-  "COVID-19 Dashboard"
+
+N <- 1
+
+conceptFile <- "images/concept.png"
+databaseName <- "data/covid-19.db"
+prjNameShort <- "Covid-19 | Dashboard"
+prjNameFull <- "Covid-19 | Dashboard"
+
+tweet_pattern = "\\<U\\+[0-9a-zA-Z]*\\>"
+
+g_sg_dorscon = "Orange"
+
+
+## Token for displaying map
+mapToken = ''
+
+## whatever name you assigned to your created app
+appname <- "Covid19"
+
+## api key
+key <- ""
+
+## api secret
+secret <- ""
+
+## access token 
+access_token <- ""
+
+## access secret
+access_secret <- ""
+
---
+++ components/tab_0103_ui.R
@@ -0,0 +1,8 @@
+div(
+  wellPanel(
+    plotOutput(outputId = "word_freq", height = "1200px"),
+    h6("The wordcloud provides the most frequently used words in Twitter. Based on the words, the hot topic of that day can be inferred.", style="color: #BDCED6;")
+  )
+)
+
+
